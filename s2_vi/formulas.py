9db6f404e0422e483e6cffa0efcd725ffa716d19
Christoph Friedrich <christoph.friedrich@uni-wuerzburg.de>
2024-08-23 12:30:00 +0200

    Switch crop stat retrieval to new NetCDF file

--- s2_vi/formulas.py
+++ s2_vi/formulas.py
@@ -1,17 +1,16 @@
-from ows_refactored.s2_vi.constants import ndvi_means, ndvi_stds
-from ows_refactored.s2_vi.constants import evi_means, evi_stds
+from ows_refactored.s2_vi.utils import get_crop_stats
 
 def ndvi(data):
     castdata = data.astype("float32")
     return (castdata["nir"] - castdata["red"]) / (castdata["nir"] + castdata["red"])
 
 def ndvi_diff(data, croptype):
-    mm_dd = data.time.values[0].astype(str)[5:10]
-    return ndvi(data) - ndvi_means[croptype][mm_dd]  # value applicable for combination of date and crop
+    mean, _ = get_crop_stats(data, croptype, 'con', 'ndvi')
+    return ndvi(data) - mean
 
 def ndvi_diff_norm(data, croptype):
-    mm_dd = data.time.values[0].astype(str)[5:10]
-    return ndvi_diff(data, croptype) / ndvi_stds[croptype][mm_dd]  # value applicable for combination of date and crop
+    _, std = get_crop_stats(data, croptype, 'con', 'ndvi')
+    return ndvi_diff(data, croptype) / std
 
 def evi(data):
     castdata = data.astype("float32")
@@ -23,9 +22,9 @@ def evi(data):
     return G * ((normdata["nir"] - normdata["red"]) / (normdata["nir"] + C1*normdata["red"] - C2*normdata["blue"] + L))
 
 def evi_diff(data, croptype):
-    mm_dd = data.time.values[0].astype(str)[5:10]
-    return evi(data) - evi_means[croptype][mm_dd]  # value applicable for combination of date and crop
+    mean, _ = get_crop_stats(data, croptype, 'con', 'evi')
+    return evi(data) - mean
 
 def evi_diff_norm(data, croptype):
-    mm_dd = data.time.values[0].astype(str)[5:10]
-    return evi_diff(data, croptype) / evi_stds[croptype][mm_dd]  # value applicable for combination of date and crop
+    _, std = get_crop_stats(data, croptype, 'con', 'evi')
+    return evi_diff(data, croptype) / std
--- s2_vi/utils.py
+++ s2_vi/utils.py
@@ -1,5 +1,6 @@
 import json
 from shapely.geometry import shape, Point
+import xarray as xr
 
 def get_landscape(data):
     # for simplicity: use coordinate of point in the center of the bbox
@@ -21,3 +22,10 @@ def get_landscape(data):
         if polygon.contains(centerpoint):
             return feature['properties']['ZUS']
     raise Exception("Center point of bbox must be within a Naturraum polygon - try zooming in more or panning to a different location.")
+
+def get_crop_stats(data, croptype, treatment, index):
+    stats = xr.load_dataset('/home/datacube/ows_refactored/s2_vi/DLR_crop_statistics/crop_statistics.nc')
+    landscape = get_landscape(data)
+    timeline = stats.sel(landscape=landscape, crop=croptype, treatment=treatment, index=index)
+    day = timeline.sel({'date': data.time.values[0]}, method = "nearest")
+    return day.means.item(), day.stds.item()
