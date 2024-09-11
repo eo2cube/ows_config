5684bb1165f2387b2c37f71b23561f3885909d22
Christoph Friedrich <christoph.friedrich@uni-wuerzburg.de>
2024-09-11 11:11:43 +0200

    Fix GetFeatureInfo requests

--- s2_vi/utils.py
+++ s2_vi/utils.py
@@ -5,12 +5,12 @@ import xarray as xr
 def get_landscape(data):
     # for simplicity: use coordinate of point in the center of the bbox
     if data.crs == 'EPSG:4326':
-        lon = data.longitude.values[data.longitude.size // 2]
-        lat = data.latitude.values[data.latitude.size // 2]
+        lon = data.longitude.values if data.longitude.size == 1 else data.longitude.values[data.longitude.size // 2]
+        lat = data.latitude.values if data.latitude.size  == 1 else data.latitude.values[data.latitude.size // 2]
         centerpoint = Point(lon, lat)
     elif data.crs == 'EPSG:3857':
-        x = data.x.values[data.x.size // 2]
-        y = data.y.values[data.y.size // 2]
+        x = data.x.values if data.x.size == 1 else data.x.values[data.x.size // 2]
+        y = data.y.values if data.y.size == 1 else data.y.values[data.y.size // 2]
         centerpoint = Point(x, y)
     else:
         raise Exception("Only requests in EPSG:4326 or 3857 are supported.")
@@ -27,5 +27,5 @@ def get_crop_stats(data, croptype, treatment, index):
     stats = xr.load_dataset('/home/datacube/ows_refactored/s2_vi/DLR_crop_statistics/crop_statistics.nc')
     landscape = get_landscape(data)
     timeline = stats.sel(landscape=landscape, crop=croptype, treatment=treatment, index=index)
-    day = timeline.sel({'date': data.time.values[0]}, method = "nearest")
+    day = timeline.sel({'date': data.time.values if data.time.size == 1 else data.time.values[0]}, method = "nearest")
     return day.means.item(), day.stds.item()
